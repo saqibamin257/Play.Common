e081746057561b1bae9fc92df99edfc74ffb4b21
saqibamin257 <saqibamin257@gmail.com>
2025-12-12 23:21:00 +0500

    Initial Commit

---
+++ src/Play.Common/IRepository.cs
@@ -0,0 +1,24 @@
+
+using System.Linq.Expressions;
+
+namespace Play.Common
+{
+    // public interface IItemsRepository
+    // {
+    //     Task CreateAsync(Item entity);
+    //     Task<IReadOnlyCollection<Item>> GetAllAsync();
+    //     Task<Item> GetAsync(Guid id);
+    //     Task RemoveAsync(Guid id);
+    //     Task UpdateAsync(Item entity);
+    // }
+    public interface IRepository<T> where T : IEntity
+    {
+        Task CreateAsync(T entity);
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task<IReadOnlyCollection<T>> GetAllAsync(Expression<Func<T,bool>> filter);
+        Task<T> GetAsync(Guid id);
+        Task<T> GetAsync(Expression<Func<T,bool>> filter); 
+        Task RemoveAsync(Guid id);
+        Task UpdateAsync(T entity);
+    }
+}
